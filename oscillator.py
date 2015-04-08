d37f3738869d80c1efc57e45c7ccef9be1dc3a64
Trevor Bekolay <tbekolay@gmail.com>
2015-04-08 02:42:18 -0400

    Taking into first round of feedback.
    Still a few things todo; they're in notes.org.

--- oscillator.py
+++ oscillator.py
@@ -1,14 +1,15 @@
 
 import nengo
+import nengo_viz
 import numpy as np
 
-model = nengo.Network('Oscillator', seed=1)
+model = nengo.Network('Oscillator', seed=0)
 
 tau = 0.01
 r = 4
 
 def feedback(x):    
-    return [-tau*r*x[1]+x[0], tau*r*x[0]+x[1]]
+    return [-tau*r*x[1]+1.01*x[0], tau*r*x[0]+1.01*x[1]]
 
 def osc_shape(x):
     theta = np.arctan2(x[1], x[0])
@@ -16,10 +17,14 @@ def osc_shape(x):
     return -r*np.cos(theta), r*np.sin(theta)
 
 with model:
-    stim = nengo.Node(lambda t: [.5,.5] if t<.02 else [0,0])    
+    stim = nengo.Node(0)
     oscillator = nengo.Ensemble(1000, dimensions=2)
     shape = nengo.Ensemble(100, dimensions=2, radius=4)
     
-    nengo.Connection(stim, oscillator)
+    nengo.Connection(stim, oscillator[0])
+    nengo.Connection(stim, oscillator[1])
     nengo.Connection(oscillator, oscillator, function=feedback, synapse=tau)
     nengo.Connection(oscillator, shape, function=osc_shape, synapse=tau)
+
+if __name__ == '__main__':
+    nengo_viz.Viz(__file__).start()
